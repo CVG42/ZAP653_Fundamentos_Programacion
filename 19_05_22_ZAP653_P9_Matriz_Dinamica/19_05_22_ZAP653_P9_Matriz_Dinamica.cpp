67e341e2c96ba6eaffb0b6f3eb1a03f00f6ba0fb
CVG42 <99357695+CVG42@users.noreply.github.com>
2022-05-25 09:50:59 -0700

    19_05_22_ZAP653_P9_Matriz_Dinamica
    Practica #9 Matriz 2x3 y Matriz dinamica

--- 19_05_22_ZAP653_P9_Matriz_Dinamica/19_05_22_ZAP653_P9_Matriz_Dinamica.cpp
+++ 19_05_22_ZAP653_P9_Matriz_Dinamica/19_05_22_ZAP653_P9_Matriz_Dinamica.cpp
@@ -1,4 +1,5 @@
-// 19_05_22_ZAP653_P9_Matriz_Dinamica.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// 19_05_22_ZAP653_P9_Matriz_Dinamica.cpp : Cristina Vera Garcia
+// En esta practica se va a mostrar una matriz 2x3 y una matriz dinamica donde el usuario ingresara las filas y columnas que quiere que la matriz tenga
 //
 
 #include <iostream>
@@ -7,16 +8,45 @@
 int main()
 {
 	int** matriz;
-	int filas, columnas;
+	int matriz1[2][3],filas, columnas;
 	bool ans;
+
+	std::cout << "Estos son los datos aleatorios de una matriz 2x3: \n";
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			matriz1[i][j] = rand() % 9;
+			std::cout << "[" << i << "]" << "[" << j << "] = " << matriz1[i][j];
+			std::cout << "\n";
+		}
+	}
+
+	std::cout << "\nLa matriz 2x3 generada es: \n";
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << matriz1[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+
+	system("pause");
+	system("CLS");
+
+	std::cout << "Matriz dinamica\n";
+	std::cout << "\n";
 
 	do {
 
-		std::cout << "Cuantas filas quieres?\n";
+		std::cout << "Cuantas filas deseas que tenga la matriz?\n";
 		std::cin >> filas;
-		std::cout << "Cuantas columnas quieres?\n";
+		std::cout << "Cuantas columnas deseas que tenga la matriz?\n";
 		std::cin >> columnas;
+		
 		matriz = new int* [filas];
+		//Si la matriz es de igual o mayor dimension a 4x4
 		if (filas >= 4 && columnas >= 4)
 		{
 		for (int i = 0; i < filas; i++)
@@ -24,7 +54,7 @@ int main()
 			matriz[i] = new int[columnas];
 		}
 
-		std::cout << "\n" << "Los elementos de su matriz son: \n";
+		std::cout << "\n" << "Los elementos de la matriz " << filas << "x" << columnas << " son: \n";
 		for (int i = 0; i < filas; i++)
 		{
 			for (int j = 0; j < columnas; j++)
@@ -43,9 +73,9 @@ int main()
 				std::cout << matriz[i][j] << " ";
 			}
 			std::cout << "\n";
-
 		}
 
+		//para cuando la matriz es de dimensiones 3x3 o menor
 		}
 		else {
 			for (int i = 0; i < filas; i++)
@@ -53,7 +83,7 @@ int main()
 				matriz[i] = new int[columnas];
 			}
 
-			std::cout << "\n" << "Ingresa los valores de la matriz en cada una de sus posiciones: \n";
+			std::cout << "\n" << "Ingresa los valores de la matriz para cada posicion: \n";
 			for (int i = 0; i < filas; i++)
 			{
 				for (int j = 0; j < columnas; j++)
@@ -78,7 +108,7 @@ int main()
 		}
 		delete[] matriz;
 
-		std::cout << "\n" << "Escribe 1 si quieres volver a intentarlo" << "\n";
+		std::cout << "\n" << "Para volver a intentarlo escribe 1: " << "\n";
 		std::cin >> ans;
 		std::cout << "\n";
 	} while (ans == TRUE);
