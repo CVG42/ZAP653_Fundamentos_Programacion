1bb02a0db2930d1e922cb36815d58cb703079cde
CVG42 <99357695+CVG42@users.noreply.github.com>
2022-06-02 12:13:21 -0500

    Update 26_04_22_ZAP653_P8_Inventario_JuegosdeMesa.cpp

--- 26_04_22_ZAP653_P8_Inventario_JuegosdeMesa/26_04_22_ZAP653_P8_Inventario_JuegosdeMesa.cpp
+++ 26_04_22_ZAP653_P8_Inventario_JuegosdeMesa/26_04_22_ZAP653_P8_Inventario_JuegosdeMesa.cpp
@@ -9,24 +9,37 @@ int main()
     std::string nombre[5];
     int year[5];
     std::string autor[5];
-    int i, ans;
+    int ans;
 
     std::cout << "Se va a crear una libreria de juegos de mesa";
     do
     {
-        for (i = 0; i <= 4; i++)
+        for (int i = 0; i <= 4; i++)
         {
-            std::cout << "Ingresa el titlo del juego de mesa #" << i + 1 << "\n";
+            std::cout << "\nIngresa el titlo del juego de mesa #" << i + 1 << "\n";
             std::cin.ignore();
             std::getline(std::cin, nombre[i]);
             std::cout << "Ingresa el nombre del autor del juego de mesa #" << i + 1 << "\n";
             std::getline(std::cin, autor[i]);
             std::cout << "Ingresa el año de lanzamiento del juego de mesa #" << i + 1 << "\n";
-            std::cin >> year[i];
+            while (true)
+            {
+                std::cin >> year[i];
+                if (!std::cin.good())
+                {
+                    std::cout << "Ingresa un numero entero";
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<int>::max(), '\n');
+                }
+                else
+                {
+                    break;
+                }
+            }
             std::cout << "\n";
         }
 
-        for (i = 0; i <= 4; i++)
+        for (int i = 0; i <= 4; i++)
         {
             std::cout << "\nEl titlo del juego #" << i + 1 << " es: " << nombre[i];
             std::cout << "\nEl autor del juego #" << i + 1 << " es: " << autor[i];
